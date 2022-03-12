68269f7f2e3d924929ee6c9b3b25a6a0eac782d5
LHR <LHR@DESKTOP-M05BBAF>
2022-03-12 13:11:38 +0530

    This is Likhitha commit

---
+++ src/test/java/PractiseGit2/Demo2.java
@@ -0,0 +1,13 @@
+package PractiseGit2;
+
+import org.testng.annotations.Test;
+
+public class Demo2 
+{
+
+	@Test
+	public void demo()
+	{
+		System.out.println("This is my first commit - Likhitha");
+	}
+}
